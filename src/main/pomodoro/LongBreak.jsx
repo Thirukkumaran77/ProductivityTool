c442426c1fe809d8d06871beee13df5460841ea0
unknown <kumaran77ishere@gmail.com>
2021-06-23 22:33:43 +0530

    some test change

--- src/main/pomodoro/LongBreak.jsx
+++ src/main/pomodoro/LongBreak.jsx
@@ -19,6 +19,10 @@ const Break = ({ setTimer }) => {
     alert("break time is over... work starts ");
     setTimer(true);
   }
+
+  /**
+   * Adding some dummy changes
+   */
   const min = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
   const sec = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
   return (
